af8ed93347448ed1cf44f11d615a980f98c7b097
MasakiYoshimoto <deluxeparm@gmail.com>
2017-12-23 02:36:25 +0900

    cleate add Assets

--- js/draggable.js
+++ js/draggable.js
@@ -1,3 +1,18 @@
 $(function() {
-  $(".draggable").draggable({ containment: "#contents" });
+  var count = $('.palet').find('.draggable').length;
+  if(count==0){
+    $('.none-asset').css('display', 'flex');
+  }
+  do_draggable();
 });
+
+function add_assets(){
+  var count = $('#contents').find('.draggable').length;
+  $('.none-asset').css('display', 'none');
+  $('.palet').append('<div class="draggable b-red ui-draggable">要素'+(count+=1)+'</div>');
+  do_draggable();
+}
+
+function do_draggable(){
+  $(".draggable").draggable({ containment: ".palet" });
+}
