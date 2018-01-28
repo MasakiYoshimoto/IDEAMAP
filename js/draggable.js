a41e95fc6b7f7dc660ba3a08e7589a66fd429594
MasakiYoshimoto <deluxeparm@gmail.com>
2018-01-28 09:48:52 +0900

    change color

--- js/draggable.js
+++ js/draggable.js
@@ -7,9 +7,11 @@ $(function() {
 });
 
 function add_assets(input_txt){
+  input_txt = input_txt.replace(/\r?\n/g,"<br>");
+  var b_color = get_random_bg_color();
   var count = $('#contents').find('.draggable').length;
   $('.none-asset').css('display', 'none');
-  $('.palet').append('<div class="draggable b-red ui-draggable asset-'+(count+=1)+'">'+input_txt+'</div>');
+  $('.palet').append('<div class="draggable '+b_color+' ui-draggable circle asset-'+(count+=1)+'">'+input_txt+'</div>');
   do_draggable();
 }
 
@@ -22,7 +24,6 @@ $(document).ready(function(){
     $(".input-modal").fadeIn(700).css('display','table');
   });
 
-
   $(".input-modal").on('click touchend',function(event){
     if(!$(event.target).closest('.inner-item').length){
       $(".input-modal").fadeOut(700);
@@ -32,3 +33,34 @@ $(document).ready(function(){
     }
   });
 });
+
+function get_random_b_color(){
+  var b_color = "";
+  var arr_b_color = [
+    "b-brown","b-gray",
+    "b-light-brown","b-pale-purple","b-pale-plum",
+    "b-pale-ocher","b-pale-beige","b-blue-gray",
+    "b-pale-green","b-red-brown","b-ice-gray",
+    "b-pale-yellow-green"
+  ];
+  b_color = get_random_value(arr_b_color);
+  return b_color;
+}
+
+function get_random_bg_color(){
+  var bg_color = "";
+  var arr_bg_color = [
+    "bg-brown","bg-gray",
+    "bg-light-brown","bg-pale-purple","bg-pale-plum",
+    "bg-pale-ocher","bg-pale-beige","bg-blue-gray",
+    "bg-pale-green","bg-red-brown","bg-ice-gray",
+    "bg-pale-yellow-green"
+  ];
+  bg_color = get_random_value(arr_bg_color);
+  return bg_color;
+}
+
+function get_random_value(array){
+  var value = array[Math.floor(Math.random() * array.length)];
+  return value;
+}
